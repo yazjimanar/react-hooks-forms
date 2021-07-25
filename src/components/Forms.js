de893f25a6af85996250c47993e5745913b84fe8
yazjimanar <m.gmail.com>
2021-07-25 21:26:55 +0300

    hi

---
+++ src/components/Forms.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Form(props) {
+  return (
+    <form>
+      <input
+        type="text"
+        onChange={props.handleFirstNameChange}
+        value={props.firstName}
+      />
+      <input
+        type="text"
+        onChange={props.handleLastNameChange}
+        value={props.lastName}
+      />
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
+
+export default Form;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Form from './components/Form'
+import React from "react";
+import ReactDOM from "react-dom";
+import ParentComponent from "./components/ParentComponent";
 
-ReactDOM.render(
-  <Form />,
-  document.getElementById('root')
-);
+ReactDOM.render(<ParentComponent />, document.getElementById("root"));
